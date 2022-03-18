3cbf2e5dec226b9fd9c14c0e5be4de79237d3d18
ppaanngggg <hantian.pang@gmail.com>
2022-03-18 23:44:49 +0800

    [+] add awt runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,16 +9,46 @@ version = "0.1.0-SNAPSHOT"
 
 repositories { mavenCentral() }
 
+val osName: String = System.getProperty("os.name")
+val targetOs =
+    when {
+      osName == "Mac OS X" -> "macos"
+      osName.startsWith("Win") -> "windows"
+      osName.startsWith("Linux") -> "linux"
+      else -> error("Unsupported OS: $osName")
+    }
+
+var targetArch =
+    when (val osArch: String = System.getProperty("os.arch")) {
+      "x86_64", "amd64" -> "x64"
+      "aarch64" -> "arm64"
+      else -> error("Unsupported arch: $osArch")
+    }
+
+val target = "${targetOs}-${targetArch}"
+val skikoVersion = "0.7.16"
+
 kotlin {
   jvm {
     compilations.all { kotlinOptions.jvmTarget = "1.8" }
     withJava()
-    testRuns["test"].executionTask.configure { useJUnitPlatform() }
   }
   sourceSets {
-    val commonMain by getting
-    val commonTest by getting { dependencies { implementation(kotlin("test")) } }
-    val jvmMain by getting
-    val jvmTest by getting
+    val commonMain by getting { dependencies { api("org.jetbrains.skiko:skiko:$skikoVersion") } }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test-common"))
+        implementation(kotlin("test-annotations-common"))
+      }
+    }
+    val jvmMain by getting {
+      dependencies { implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$skikoVersion") }
+    }
+    val jvmTest by getting {
+      dependencies {
+        implementation(kotlin("test-junit"))
+        implementation(kotlin("test"))
+      }
+    }
   }
 }
---
+++ src/jvmTest/kotlin/io/github/ppaanngggg/kcharts/KChartsTest.kt
@@ -0,0 +1,29 @@
+package io.github.ppaanngggg.kcharts
+
+import org.jetbrains.skia.*
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class KChartsTest {
+
+  private val width = 100
+  private val height = 100
+  private lateinit var bitmap: Bitmap
+  lateinit var canvas: Canvas
+
+  @BeforeTest
+  fun init() {
+    bitmap = Bitmap()
+    assertTrue {
+      bitmap.allocPixels(ImageInfo(width, height, ColorType.RGBA_8888, ColorAlphaType.UNPREMUL))
+    }
+    canvas = Canvas(bitmap)
+  }
+
+  @Test
+  fun draw() {
+    canvas.drawLine(0f, 0f, 99f, 99f, Paint().setARGB(255, 255, 255, 255))
+    val image = Image.makeFromBitmap(bitmap)
+  }
+}
